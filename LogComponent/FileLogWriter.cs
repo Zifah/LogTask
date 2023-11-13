2257d4b050e3a3d54e7e7107367736deb801987a
Hafiz Adewuyi <hoadewuyi@gmail.com>
2023-11-13 21:03:25 +0200

    Disable StreamWriter re-use due to poorer perf.

--- LogComponent/FileLogWriter.cs
+++ LogComponent/FileLogWriter.cs
@@ -19,106 +19,46 @@ namespace LogComponent
         /// <summary>
         /// Prevents <see cref="_currentWriter"> from being used for writing while it is being disposed
         /// </summary>
-        private static readonly object _writerManagementLock = new();
-
-        private readonly ConcurrentDictionary<StreamWriter, List<Guid>> _writeLocks;
-
+        private static readonly object _lockObject = new();
         public FileLogWriter(IClock clock)
         {
             _clock = clock;
             _curDate = _clock.CurrentTime;
-            _writeLocks = new ConcurrentDictionary<StreamWriter, List<Guid>>();
 
             if (!Directory.Exists(LogFolder))
                 Directory.CreateDirectory(LogFolder);
             InitializeLogFile();
         }
 
-        private bool _isCurrentWriterDisposed = false;
-        private StreamWriter? _currentWriter;
-        private StreamWriter GetWriter(Guid lockKey)
+        private StreamWriter Writer
         {
-            lock (_writerManagementLock)
+            get
             {
-                var createNewWriter = _currentWriter == null || _isCurrentWriterDisposed || EnsureFileRollOver();
-                if (createNewWriter)
-                {
-                    var writer = File.AppendText(LogFilePath);
-                    writer.AutoFlush = true;
-                    TakeWriteLock(writer, lockKey);
-
-                    Timer countdownTimer = new(o => TryDisposeWriter(o));
-                    _currentWriter = writer;
-                    _isCurrentWriterDisposed = false;
-                    countdownTimer.Change(WriterIdleTimeoutMs, WriterIdleTimeoutMs);
-                }
-                return _currentWriter;
+                EnsureFileRollOver();
+                var writer = File.AppendText(LogFilePath);
+                writer.AutoFlush = true;
+                return writer;
             }
         }
-
-        private void TryDisposeWriter(object? state)
-        {
-            if (state is not Timer timer || _currentWriter is null)
-            {
-                return; // Unexpected, should log
-            }
-
-            if (!_writeLocks[_currentWriter].Any() && !_isCurrentWriterDisposed)
-            {
-                lock (_writerManagementLock)
-                {
-                    using (timer)
-                    {
-                        using (_currentWriter)
-                        {
-                            _isCurrentWriterDisposed = true;
-                        }
-
-                        timer.Change(Timeout.Infinite, Timeout.Infinite);
-                    }
-                }
-                Console.WriteLine("Successfully disposed of current writer and timer");
-            }
-            else
-            {
-                Console.WriteLine("Writer is busy");
-            }
-        }
-
         public void Write(string logText)
         {
-            var lockKey = Guid.NewGuid();
-            var writer = GetWriter(lockKey);
-
-            try
-            {
-                writer.Write(logText);
-            }
-            finally
-            {
-                ReleaseWriteLock(writer, lockKey);
-            }
-        }
-
-        private void ReleaseWriteLock(StreamWriter writer, Guid lockKey)
-        {
-            _writeLocks[writer].Remove(lockKey);
-        }
-
-        private void TakeWriteLock(StreamWriter writer, Guid lockKey)
-        {
-            // Implement your logic to generate and return a GUID here
-            _writeLocks.TryAdd(writer, new List<Guid>());
-            _writeLocks[writer].Add(lockKey);
+            using var writer = Writer;
+            writer.AutoFlush = true;
+            writer.Write(logText);
         }
 
         private void InitializeLogFile()
         {
-            // TODO Hafiz: Make thread-safe
             if (!File.Exists(LogFilePath))
             {
-                File.WriteAllText(LogFilePath,
-               "Timestamp".PadRight(25, ' ') + '\t' + "Data".PadRight(15, ' ') + '\t' + Environment.NewLine);
+                lock (_lockObject)
+                {
+                    if (!File.Exists(LogFilePath))
+                    {
+                        File.WriteAllText(LogFilePath,
+                       "Timestamp".PadRight(25, ' ') + '\t' + "Data".PadRight(15, ' ') + '\t' + Environment.NewLine);
+                    }
+                }
             }
         }
 
